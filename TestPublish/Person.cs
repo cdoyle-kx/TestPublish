2c577c209ab9fd0417a2723fe86fb2943aeb0fbf
Conor Doyle <cdoyle4@firstderivatives.com>
2020-11-26 12:50:35 +0000

    Initial check-in

---
+++ TestPublish/Person.cs
@@ -0,0 +1,18 @@
+﻿namespace TestPublish
+{
+    /// <summary>
+    /// Dummy class representing a person object.
+    /// </summary>
+    public class Person
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the age.
+        /// </summary>
+        public int Age { get; set; }
+    }
+}
